7fdefa1b18872d7f3b96ada02e28f8de1ff2ea5a
Parker Harris <parker.harris@intel.com>
2023-10-03 13:08:36 -0700

    Constrains worker threads to QAT_ZSTD_WORKERS

--- lib/encoding/zstd/zstd_cgo.go
+++ lib/encoding/zstd/zstd_cgo.go
@@ -5,61 +5,107 @@ package zstd
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"sync"
 
 	"github.com/intel/qatgo/qatzip"
 	"github.com/valyala/gozstd"
 )
+
+const (
+	envWorkers     = "QAT_ZSTD_WORKERS"
+	DefaultWorkers = 8
+)
+
+var (
+	once     sync.Once
+	sessions []session
+)
+
+type session struct {
+	binding *qatzip.QzBinding
+	m       sync.Mutex
+}
+
+func acquireBinding() int {
+	for {
+		for a := range sessions {
+			if sessions[a].m.TryLock() {
+				return a
+			}
+		}
+	}
+}
 
 // Decompress appends decompressed src to dst and returns the result.
 func Decompress(dst, src []byte) ([]byte, error) {
 	return gozstd.Decompress(dst, src)
 }
 
+func initBinding() {
+	var err error
+	var workerThreads int
+	workerThreadsStr := os.Getenv(envWorkers)
+	if workerThreadsStr != "" {
+		if workerThreads, err = strconv.Atoi(workerThreadsStr); err != nil {
+			panic("Invalid " + envWorkers + " value")
+		}
+	} else {
+		workerThreads = DefaultWorkers
+	}
+	sessions = make([]session, workerThreads)
+	for x := 0; x < len(sessions); x++ {
+		binding, err := qatzip.NewQzBinding()
+		if err != nil {
+			fmt.Printf("BINDING ERROR: %v\n", err)
+			panic("Error")
+		}
+		err = binding.Apply(qatzip.AlgorithmOption(qatzip.ZSTD))
+		if err != nil {
+			fmt.Printf("OPTIONS ERROR: %v\n", err)
+			panic("Error")
+		}
+		err = binding.StartSession()
+		if err != nil {
+			fmt.Printf("SESSION ERROR: %v\n", err)
+			panic("Error")
+		}
+		binding.SetLast(true)
+		sessions[x].binding = binding
+	}
+}
+
 // CompressLevel appends compressed src to dst and returns the result.
 //
 // The given compressionLevel is used for the compression.
 func CompressLevel(dst, src []byte, compressionLevel int) []byte {
-	if cap(dst) == 0 {
+	once.Do(initBinding)
+	dstLen := len(dst)
+	if cap(dst)-len(dst) == 0 {
 		dst = append(dst, make([]byte, len(src))...)
 	}
 
-	binding, err := qatzip.NewQzBinding()
-	if err != nil {
-		fmt.Printf("error: %v\n", err)
-		panic("Error")
-	}
-	err = binding.Apply(qatzip.AlgorithmOption(qatzip.ZSTD))
-	if err != nil {
-		fmt.Printf("OPTIONS ERROR: %v\n", err)
-		panic("Error")
-	}
-	err = binding.StartSession()
-	if err != nil {
-		fmt.Printf("SESSION ERROR: %v\n", err)
-		panic("Error")
-	}
 	dst = dst[:cap(dst)]
-	binding.SetLast(true)
 
 	for {
-		_, out, err := binding.Compress(src, dst)
+		sessionID := acquireBinding()
+		binding := sessions[sessionID].binding
+		binding.Apply(qatzip.CompressionLevelOption(compressionLevel))
+		_, out, err := binding.Compress(src, dst[dstLen:])
+		sessions[sessionID].m.Unlock()
 		if err != nil {
 			if err == qatzip.ErrBuffer {
-				dst = append(dst, make([]byte, len(dst))...)
-				//log.Panicln("Panic: resize src: ", len(src), " dst: ", len(dst2))
+				dst = append(dst, make([]byte, cap(dst)+len(src))...)
 				continue
 			} else {
 				fmt.Printf("COMPRESS ERROR: %v\n", err)
-				fmt.Printf("Location of first src entry: %p\n", &src[0])
-				fmt.Printf("Location of first dst entry: %p\n", &dst[0])
-
 				panic("Error")
 			}
 		}
-		dst = dst[:out]
+		dst = dst[:out+dstLen]
 		break
 	}
-	//binding.Close() //Currently this Close() will cause VM to crash, still mostly functions without it 
 	return dst
 	//return gozstd.CompressLevel(dst, src, compressionLevel)
 }
